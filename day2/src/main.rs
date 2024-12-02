bcb9f654cbf2c6476989677b368bf2819a175c25
Zac Swider <zac@concertobio.com>
2024-12-02 11:26:20 -0600

    start day2

---
+++ day2/src/main.rs
@@ -0,0 +1,8 @@
+fn load_input(input_path: &str) -> () {
+    let file = std::fs::File::open(input_path).expect("Failed to open input_path");
+    let reader = std::io::BufReader::new(file);
+}
+
+fn main() {
+    p = "/Users/zacswider/code/AOC2024/day2/days2.txt";
+}
